70d42c3405481ca848a234db1af46c0078c8fd92
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-02-13 10:35:06 +0900

    Change from getter to function

--- data/src/main/java/io/foundy/data/extension/ResponseExtension.kt
+++ data/src/main/java/io/foundy/data/extension/ResponseExtension.kt
@@ -4,24 +4,23 @@ import com.google.gson.Gson
 import io.foundy.data.model.common.ResponseBody
 import retrofit2.Response
 
-val <T> Response<ResponseBody<T>>.errorMessage: String?
-    get() {
-        if (isSuccessful) throw IllegalStateException()
+fun <T> Response<ResponseBody<T>>.getErrorMessage(): String? {
+    if (isSuccessful) throw IllegalStateException()
 
-        return try {
-            message()
-            val errorBodyJson = requireNotNull(errorBody()).string()
-            val responseBody = Gson().fromJson(errorBodyJson, ResponseBody::class.java)
-            responseBody.message
-        } catch (e: Exception) {
-            e.message
-        }
+    return try {
+        message()
+        val errorBodyJson = requireNotNull(errorBody()).string()
+        val responseBody = Gson().fromJson(errorBodyJson, ResponseBody::class.java)
+        responseBody.message
+    } catch (e: Exception) {
+        e.message
     }
+}
 
 fun <T> Response<ResponseBody<T>>.getDataOrThrowMessage(): T {
     if (isSuccessful) {
         return requireNotNull(body()).data
     } else {
-        throw Exception(errorMessage)
+        throw Exception(getErrorMessage())
     }
 }
