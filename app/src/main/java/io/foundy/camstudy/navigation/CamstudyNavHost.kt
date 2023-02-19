e0cf12a942c2ef00e1ed0273c009cf9c59ebf9c8
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-02-19 20:16:42 +0900

    Rerange imports

--- app/src/main/java/io/foundy/camstudy/navigation/CamstudyNavHost.kt
+++ app/src/main/java/io/foundy/camstudy/navigation/CamstudyNavHost.kt
@@ -20,8 +20,8 @@ import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeOut
 import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
