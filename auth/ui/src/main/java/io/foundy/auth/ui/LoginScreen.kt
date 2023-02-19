134cfbc9b35620ead7e35c3f00e9ff87f8139cbe
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-02-19 20:26:44 +0900

    Remove wildcard import

--- auth/ui/src/main/java/io/foundy/auth/ui/LoginScreen.kt
+++ auth/ui/src/main/java/io/foundy/auth/ui/LoginScreen.kt
@@ -7,8 +7,15 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
