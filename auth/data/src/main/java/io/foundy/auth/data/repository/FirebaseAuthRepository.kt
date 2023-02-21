e06f949223993bb75a7f07c289fae813bb00da70
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-02-21 10:55:41 +0900

    private 필드 제거하여 코드 단순화

--- auth/data/src/main/java/io/foundy/auth/data/repository/FirebaseAuthRepository.kt
+++ auth/data/src/main/java/io/foundy/auth/data/repository/FirebaseAuthRepository.kt
@@ -7,7 +7,6 @@ import io.foundy.auth.data.source.AuthRemoteDataSource
 import io.foundy.core.common.di.ApplicationScope
 import io.foundy.core.data.extension.getDataOrThrowMessage
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -20,11 +19,8 @@ class FirebaseAuthRepository @Inject constructor(
     private val authLocalDataSource: AuthLocalDataSource
 ) : AuthRepository {
 
-    private val _currentUserIdState: MutableSharedFlow<String?> = MutableSharedFlow(replay = 1)
-    override val currentUserIdStream: Flow<String?> get() = _currentUserIdState
-
-    private var _existsInitInfo: Boolean? = null
-    override val existsInitInfo: Boolean? get() = _existsInitInfo
+    override val currentUserIdStream: MutableSharedFlow<String?> = MutableSharedFlow(replay = 1)
+    override var existsInitInfo: Boolean? = null
 
     init {
         Firebase.auth.addAuthStateListener { auth ->
@@ -35,23 +31,23 @@ class FirebaseAuthRepository @Inject constructor(
     private fun fetchAndNotify(currentUserId: String?) {
         externalScope.launch {
             if (currentUserId == null) {
-                _existsInitInfo = null
-                _currentUserIdState.emit(null)
+                existsInitInfo = null
+                currentUserIdStream.emit(null)
                 return@launch
             }
             val existsInLocal = authLocalDataSource.existsUserInitialInfo(currentUserId)
             if (existsInLocal) {
-                _existsInitInfo = true
+                existsInitInfo = true
             } else {
                 runCatching {
                     val response = authRemoteDataSource.getUserInitialInfoExistence(currentUserId)
                     response.getDataOrThrowMessage()
                 }.onSuccess {
-                    _existsInitInfo = it
+                    existsInitInfo = it
                     authLocalDataSource.markAsUserInitialInfoExists(currentUserId)
                 }
             }
-            _currentUserIdState.emit(currentUserId)
+            currentUserIdStream.emit(currentUserId)
         }
     }
 }
--- room/data/src/main/java/io/foundy/room/data/service/RoomSocketService.kt
+++ room/data/src/main/java/io/foundy/room/data/service/RoomSocketService.kt
@@ -2,11 +2,9 @@ package io.foundy.room.data.service
 
 import io.foundy.room.data.model.OnCreated
 import io.foundy.room.data.model.RoomEvent
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class RoomSocketService : RoomService {
 
-    private val _event: MutableStateFlow<RoomEvent> = MutableStateFlow(OnCreated)
-    override val event: Flow<RoomEvent> get() = _event
+    override val event: MutableStateFlow<RoomEvent> = MutableStateFlow(OnCreated)
 }
