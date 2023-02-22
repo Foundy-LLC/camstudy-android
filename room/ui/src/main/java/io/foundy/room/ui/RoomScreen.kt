61b6ff07e89aba78fbfc392d87a86539c08e9863
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-02-22 15:21:31 +0900

    BackHandler 추가

--- room/ui/src/main/java/io/foundy/room/ui/RoomScreen.kt
+++ room/ui/src/main/java/io/foundy/room/ui/RoomScreen.kt
@@ -1,10 +1,12 @@
 package io.foundy.room.ui
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import io.foundy.room.data.model.WaitingRoomData
 import org.orbitmvi.orbit.compose.collectAsState
 
@@ -12,12 +14,19 @@ import org.orbitmvi.orbit.compose.collectAsState
 @Composable
 fun RoomRoute(
     id: String,
+    navigator: DestinationsNavigator,
     viewModel: RoomViewModel = hiltViewModel()
 ) {
     val uiState = viewModel.collectAsState().value
 
     LaunchedEffect(id) {
         viewModel.connect(id)
+    }
+
+    BackHandler {
+        // TODO: 사용자에게 한 번 더 확인하기
+        // TODO: 소켓 연결 끊기
+        navigator.popBackStack()
     }
 
     when (uiState) {
