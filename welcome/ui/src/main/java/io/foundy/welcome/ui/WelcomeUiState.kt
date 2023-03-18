7b46ba22ff88716c7d2a0bb598eebf2cbdda1822
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-03-18 18:16:09 +0900

    Change to show loading text in Welcome view

--- welcome/ui/src/main/java/io/foundy/welcome/ui/WelcomeUiState.kt
+++ welcome/ui/src/main/java/io/foundy/welcome/ui/WelcomeUiState.kt
@@ -4,7 +4,6 @@ import android.graphics.Bitmap
 import androidx.annotation.StringRes
 import io.foundy.core.model.constant.UserConstants
 
-// TODO: 제출하고 로딩중일 때 로딩을 나타내는 필드 추가하기
 data class WelcomeUiState(
     val nameInput: String = "",
     val selectedProfileImage: Bitmap? = null,
@@ -13,7 +12,8 @@ data class WelcomeUiState(
     val addedTags: List<String> = emptyList(),
     val didFocusNameInput: Boolean = false,
     @StringRes val nameErrorMessageRes: Int? = null,
-    @StringRes val tagErrorMessageRes: Int? = null
+    @StringRes val tagErrorMessageRes: Int? = null,
+    val inSaving: Boolean = false
 ) {
     val enableTagInput: Boolean
         get() = addedTags.size < UserConstants.MaxTagCount
@@ -24,5 +24,14 @@ data class WelcomeUiState(
             !addedTags.contains(tagInput)
 
     val enabledDoneButton: Boolean
-        get() = nameInput.isNotEmpty() && addedTags.isNotEmpty()
+        get() = nameInput.isNotEmpty() && addedTags.isNotEmpty() && !inSaving
+
+    @get:StringRes
+    val doneButtonTextRes: Int
+        get() {
+            if (inSaving) {
+                return R.string.saving
+            }
+            return R.string.done
+        }
 }
