ef08d9f26819c57d93df711cc3800b3aeae5183c
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-03-18 17:56:51 +0900

    Refactor to use server base url using local.properties (#4)

--- room/data/src/main/java/io/foundy/room/data/model/Protocol.kt
+++ room/data/src/main/java/io/foundy/room/data/model/Protocol.kt
@@ -2,10 +2,6 @@ package io.foundy.room.data.model
 
 internal object Protocol {
 
-    const val IP_ADDRESS = "10.0.2.2" // 10.0.2.2
-
-    const val PORT_NUMBER = "2000"
-
     /**
      * 소켓의 네임 스페이스이다.
      *
