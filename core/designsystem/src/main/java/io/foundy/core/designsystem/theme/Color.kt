a004a9806a5e87444b1288563ca0dfce8b7c1c17
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-03-30 23:56:32 +0900

    Add colors

--- core/designsystem/src/main/java/io/foundy/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/io/foundy/core/designsystem/theme/Color.kt
@@ -9,3 +9,20 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Yellow50 = Color(0xFFFCF7E8)
+val Yellow500 = Color(0xFFDFA812)
+
+val Green50 = Color(0xFFEAFAE9)
+val Green500 = Color(0xFF25C51E)
+
+val Red50 = Color(0xFFFCE8E8)
+val Red500 = Color(0xFFDC0F16)
+
+val Gray50 = Color(0xFFF0F0F0)
+val Gray400 = Color(0xFF838486)
+val Gray500 = Color(0xFF646568)
+val Gray600 = Color(0xFF4B4C4E)
+val Gray700 = Color(0xFF323334)
+val Gray800 = Color(0xFF19191A)
+val Gray900 = Color(0xFF0A0A0A)
