6f74d943a795c8312216e6b92287a43593cb3569
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-04-13 17:28:23 +0900

    Fix workflow by removing unused imports

--- room/ui/src/main/java/io/foundy/room/ui/component/ToggleIconButton.kt
+++ room/ui/src/main/java/io/foundy/room/ui/component/ToggleIconButton.kt
@@ -3,7 +3,6 @@ package io.foundy.room.ui.component
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.PlainTooltipBox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,7 +10,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import io.foundy.core.designsystem.component.CamstudyTooltipBox
 import io.foundy.core.designsystem.icon.CamstudyIcon
-import io.foundy.core.designsystem.theme.CamstudyTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
