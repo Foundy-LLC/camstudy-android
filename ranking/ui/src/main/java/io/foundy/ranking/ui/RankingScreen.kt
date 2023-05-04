1e7198a2cee424574a7f9d7b30333afb40e19d9d
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-05-04 17:58:52 +0900

    Implement ranking screen tab

--- ranking/ui/src/main/java/io/foundy/ranking/ui/RankingScreen.kt
+++ ranking/ui/src/main/java/io/foundy/ranking/ui/RankingScreen.kt
@@ -1,16 +1,78 @@
 package io.foundy.ranking.ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.ramcosta.composedestinations.annotation.Destination
+import io.foundy.core.designsystem.component.CamstudyTab
+import io.foundy.core.designsystem.component.CamstudyTabRow
 import io.foundy.core.designsystem.component.CamstudyText
+import io.foundy.core.designsystem.theme.CamstudyTheme
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 @Destination
 fun RankingRoute() {
-    RankingScreen()
+    val pagerState = rememberPagerState(0)
+
+    RankingScreen(
+        pagerState = pagerState
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun RankingScreen(
+    pagerState: PagerState
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        CamstudyTabRow(
+            selectedTabIndex = pagerState.currentPage,
+        ) {
+            for (destination in RankingTabDestination.values) {
+                val index = RankingTabDestination.indexOf(destination)
+                CamstudyTab(
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    text = stringResource(id = destination.labelRes)
+                )
+            }
+        }
+        HorizontalPager(pageCount = RankingTabDestination.values.size, state = pagerState) { page ->
+            RankingContent(tab = RankingTabDestination.values[page])
+        }
+    }
 }
 
 @Composable
-fun RankingScreen() {
-    CamstudyText(text = "랭킹화면")
+private fun RankingContent(
+    tab: RankingTabDestination
+) {
+    CamstudyText(text = stringResource(id = tab.labelRes))
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Preview
+@Composable
+fun RankingScreenPreview() {
+    val pagerState = rememberPagerState(0)
+
+    CamstudyTheme {
+        RankingScreen(
+            pagerState = pagerState
+        )
+    }
 }
---
+++ ranking/ui/src/main/java/io/foundy/ranking/ui/RankingTabDestination.kt
@@ -0,0 +1,23 @@
+package io.foundy.ranking.ui
+
+import androidx.annotation.StringRes
+
+enum class RankingTabDestination(@StringRes val labelRes: Int) {
+    Total(
+        labelRes = R.string.total_ranking_tab
+    ),
+    Weekly(
+        labelRes = R.string.weekly_ranking_tab
+    ),
+    Organization(
+        labelRes = R.string.organization_ranking_tab
+    );
+
+    companion object {
+        val values = RankingTabDestination.values()
+
+        fun indexOf(destination: RankingTabDestination): Int {
+            return values.indexOf(destination)
+        }
+    }
+}
