5d6c3dbd4efcbaeec7d0346ee6208a445c132f16
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-05-10 21:02:02 +0900

    Fix test in FakeUserRepository

--- welcome/ui/src/test/java/io/foundy/welcome/ui/fake/FakeUserRepository.kt
+++ welcome/ui/src/test/java/io/foundy/welcome/ui/fake/FakeUserRepository.kt
@@ -1,8 +1,13 @@
 package io.foundy.welcome.ui.fake
 
+import io.foundy.core.model.CropGrade
+import io.foundy.core.model.CropType
+import io.foundy.core.model.GrowingCrop
+import io.foundy.core.model.HarvestedCrop
 import io.foundy.core.model.User
 import io.foundy.user.domain.repository.UserRepository
 import java.io.File
+import java.util.Date
 
 class FakeUserRepository : UserRepository {
 
@@ -10,12 +15,33 @@ class FakeUserRepository : UserRepository {
         return Result.success(
             User(
                 id = "id",
-                name = "name",
-                introduce = "hello",
-                rankingScore = 100,
-                studyTimeSec = 60,
-                organizations = emptyList(),
-                tags = listOf("android")
+                name = "김민성",
+                introduce = "안녕하세요",
+                profileImage = null,
+                weeklyRanking = 23,
+                totalRanking = 40,
+                weeklyStudyTimeSec = 23142,
+                weeklyRankingOverall = 42,
+                growingCrop = GrowingCrop(
+                    id = "gid",
+                    ownerId = "id",
+                    type = CropType.PUMPKIN,
+                    level = 2,
+                    expectedGrade = CropGrade.GOLD,
+                    isDead = false,
+                    plantedAt = Date()
+                ),
+                harvestedCrops = listOf(
+                    HarvestedCrop(
+                        type = CropType.TOMATO,
+                        grade = CropGrade.SILVER,
+                        plantedAt = Date(),
+                        harvestedAt = Date()
+                    )
+                ),
+                organizations = listOf("한성대학교"),
+                tags = listOf("안드로이드", "개발", "웹"),
+                consecutiveStudyDays = 4
             )
         )
     }
