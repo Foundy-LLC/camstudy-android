26d9acf91dbcb6dff89750f8a4d78e8adc4525dd
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-05-16 15:21:00 +0900

    Add "개발중이에요!" text in FriendRecommendContent

--- friend/ui/src/main/java/io/foundy/friend/ui/component/FriendRecommendContent.kt
+++ friend/ui/src/main/java/io/foundy/friend/ui/component/FriendRecommendContent.kt
@@ -1,10 +1,33 @@
 package io.foundy.friend.ui.component
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import io.foundy.core.designsystem.component.CamstudyText
+import io.foundy.core.designsystem.theme.CamstudyTheme
 import io.foundy.friend.ui.FriendRecommendTabUiState
+import io.foundy.friend.ui.R
 
 @Composable
 fun FriendRecommendContent(
     uiState: FriendRecommendTabUiState
 ) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        CamstudyText(
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.Center),
+            text = stringResource(R.string.in_developmenting),
+            style = CamstudyTheme.typography.headlineSmall.copy(
+                color = CamstudyTheme.colorScheme.systemUi04,
+                fontWeight = FontWeight.SemiBold
+            )
+        )
+    }
 }
