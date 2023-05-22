0104ff333dda6098637f429bc38f7b9b51e30412
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-05-22 09:33:12 +0900

    Update UserConstants

--- core/model/src/main/java/io/foundy/core/model/constant/User.kt
+++ core/model/src/main/java/io/foundy/core/model/constant/User.kt
@@ -1,8 +1,13 @@
 package io.foundy.core.model.constant
 
 object UserConstants {
-    const val MaxNameLength = 500
-    const val MaxIntroduceLength = 500
+    const val MaxNameLength = 20
+    const val MaxIntroduceLength = 100
     const val MaxTagCount = 3
     const val MaxTagLength = 20
+
+    fun hasNameValidCharacterSet(name: String): Boolean {
+        val regex = Regex("^[a-zA-Z\\d가-힣ㄱ-ㅎㅏ-ㅣ]+$")
+        return regex.matches(name)
+    }
 }
