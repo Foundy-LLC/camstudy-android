ddf6a529d46c8500c707c623fb18de59b41db03e
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-05-26 17:22:35 +0900

    Change Strawberry requireDay to Zero

--- core/model/src/main/java/io/foundy/core/model/CropType.kt
+++ core/model/src/main/java/io/foundy/core/model/CropType.kt
@@ -3,7 +3,7 @@ package io.foundy.core.model
 enum class CropType(val requiredDay: Int, val maxLevel: Int) {
     CARROT(requiredDay = 3, maxLevel = 3),
     TOMATO(requiredDay = 5, maxLevel = 5),
-    STRAWBERRY(requiredDay = 7, maxLevel = 5),
+    STRAWBERRY(requiredDay = 0, maxLevel = 5),
     PUMPKIN(requiredDay = 9, maxLevel = 5),
     CABBAGE(requiredDay = 11, maxLevel = 5)
 }
