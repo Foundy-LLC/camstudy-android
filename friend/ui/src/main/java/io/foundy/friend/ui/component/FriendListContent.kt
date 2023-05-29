0cee3c9db7cc144d308c2566c95599530360dc9a
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-05-29 18:28:21 +0900

    Fix pull to refresh behavior in FriendScreen

--- friend/ui/src/main/java/io/foundy/friend/ui/component/FriendListContent.kt
+++ friend/ui/src/main/java/io/foundy/friend/ui/component/FriendListContent.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -42,10 +43,12 @@ fun FriendListContent(
         refreshing = isFriendListLoading,
         onRefresh = onRefresh
     ) {
-        if (users.itemCount == 0 && !isFriendListLoading) {
-            EmptyFriends()
-        } else {
-            LazyColumn(Modifier.fillMaxSize()) {
+        LazyColumn(Modifier.fillMaxSize()) {
+            if (users.itemCount == 0 && !isFriendListLoading) {
+                item {
+                    EmptyFriends()
+                }
+            } else {
                 items(
                     count = users.itemCount,
                     key = users.itemKey { it.id }
@@ -96,10 +99,10 @@ fun FriendListContent(
 }
 
 @Composable
-private fun EmptyFriends() {
+private fun LazyItemScope.EmptyFriends() {
     Box(
         modifier = Modifier
-            .fillMaxSize()
+            .fillParentMaxSize()
             .padding(16.dp)
     ) {
         CamstudyText(
--- friend/ui/src/main/java/io/foundy/friend/ui/component/RequestedFriendContent.kt
+++ friend/ui/src/main/java/io/foundy/friend/ui/component/RequestedFriendContent.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -37,10 +38,12 @@ fun RequestedFriendContent(
         refreshing = isLoading,
         onRefresh = onRefresh
     ) {
-        if (users.itemCount == 0 && !isLoading) {
-            EmptyFriends()
-        } else {
-            LazyColumn(Modifier.fillMaxSize()) {
+        LazyColumn(Modifier.fillMaxSize()) {
+            if (users.itemCount == 0 && !isLoading) {
+                item {
+                    EmptyFriends()
+                }
+            } else {
                 items(
                     count = users.itemCount,
                     key = users.itemKey { it.id }
@@ -78,10 +81,10 @@ fun RequestedFriendContent(
 }
 
 @Composable
-private fun EmptyFriends() {
+private fun LazyItemScope.EmptyFriends() {
     Box(
         modifier = Modifier
-            .fillMaxSize()
+            .fillParentMaxSize()
             .padding(16.dp)
     ) {
         CamstudyText(
